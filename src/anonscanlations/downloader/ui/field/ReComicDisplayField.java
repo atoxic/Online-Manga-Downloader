b6774f34281e6d54ee76e32ec42f92ded0f81e09
/a/non <anonymousscanlations@gmail.com>
2010-12-24 20:52:22 -0800

    Fixed the ReComic field's formatter

--- src/anonscanlations/downloader/ui/field/ReComicDisplayField.java
+++ src/anonscanlations/downloader/ui/field/ReComicDisplayField.java
@@ -25,6 +25,8 @@ public class ReComicDisplayField extends PrefixedDisplayField
 
     protected JFormattedTextField.AbstractFormatter getFormatter()
     {
+        return(new ReComicFormatter());
+        /*
         try
         {
             MaskFormatter formatter = new MaskFormatter("UUU-U#U########");
@@ -36,6 +38,7 @@ public class ReComicDisplayField extends PrefixedDisplayField
             DownloaderUtils.error("Couldn't parse MaskFormatter", e, false);
         }
         return(null);
+        //*/
     }
 
     @Override
@@ -65,5 +68,88 @@ public class ReComicDisplayField extends PrefixedDisplayField
             return("");
         else
             return(value);
+    }
+}
+
+class ReComicFormatter extends JFormattedTextField.AbstractFormatter
+{
+    private boolean isReComicID(String text)
+    {
+        if(!text.startsWith("PGN-A3W"))
+            return(false);
+
+        try
+        {
+            Integer.parseInt(text.substring(7));
+        }
+        catch(NumberFormatException nfe)
+        {
+            return(false);
+        }
+
+        return(true);
+    }
+
+    @Override
+    protected DocumentFilter getDocumentFilter()
+    {
+        return(new DocumentFilter()
+        {
+            private String getText(DocumentFilter.FilterBypass fb)
+                                    throws BadLocationException
+            {
+                return(fb.getDocument().getText(0, fb.getDocument().getLength()));
+            }
+
+            @Override
+            public void remove(DocumentFilter.FilterBypass fb,
+                                int offset, int length)
+                                    throws BadLocationException
+            {
+                String result = getText(fb);
+                result = result.substring(0, offset) + result.substring(offset + length);
+                if(isReComicID(result))
+                    fb.remove(offset, length);
+            }
+
+            @Override
+            public void insertString(DocumentFilter.FilterBypass fb,
+                                int offset, String string, AttributeSet attr)
+                                    throws BadLocationException
+            {
+                String result = getText(fb);
+                result = result.substring(0, offset) + string + result.substring(offset);
+                if(isReComicID(result))
+                    fb.insertString(offset, string, attr);
+            }
+
+            @Override
+            public void replace(DocumentFilter.FilterBypass fb,
+                                int offset, int length,
+                                String text, AttributeSet attrs)
+                                    throws BadLocationException
+            {
+                String result = getText(fb);
+                result = result.substring(0, offset) + text + result.substring(offset + length);
+                if(isReComicID(result))
+                    fb.replace(offset, length, text, attrs);
+            }
+        });
+    }
+
+    public Object stringToValue(String text)
+    {
+        if(!isReComicID(text))
+            return(null);
+        return(text);
+    }
+    public String valueToString(Object obj)
+    {
+        if(!(obj instanceof String))
+            return(null);
+        String text = (String)obj;
+        if(!isReComicID(text))
+            return(null);
+        return(text);
     }
 }
