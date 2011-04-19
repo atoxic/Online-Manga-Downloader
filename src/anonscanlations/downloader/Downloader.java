312e99dccf558cba01ba7d8541d8bbe0bb7413f8
/a/non <anonymousscanlations@gmail.com>
2011-04-19 16:54:04 -0700

    Cleaned up CrochetTime a bit

---
+++ src/anonscanlations/downloader/Downloader.java
@@ -0,0 +1,21 @@
+/*
+ * Coded by /a/non, for /a/non
+ */
+
+package anonscanlations.downloader;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Downloader
+{
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame("Online Manga Downloader");
+    }
+}
